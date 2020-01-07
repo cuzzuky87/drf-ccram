f8df04bf5ecfbea348c5d96630bbce613dfac305
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-01-07 22:36:37 +0900

    viewsetとserializerの調整が必要

--- calendar_events/serializers.py
+++ calendar_events/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
 from calendar_events.models import CalendarEvent
+from accounts.serializers import CustomUserSerializer
 
 class CalendarEventSerializer(serializers.Serializer):
     """Serializer for Calendar Event object"""
-
     class Meta:
         model = CalendarEvent
-        field = ('title','description','user','start_at','end_at','created_at','modified_at')
+        fields = (
+            'id',
+            'title',
+            'description',
+            'user',
+            'start_at',
+            'end_at',
+            'created_at',
+            'modified_at',
+        )
--- calendar_events/views.py
+++ calendar_events/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 
 from calendar_events.serializers import CalendarEventSerializer
 from calendar_events.models import CalendarEvent
 
 
 class CalendarEventViewSet(ModelViewSet):
-    serializer_class = CalendarEventSerializer
     queryset = CalendarEvent.objects.all()
+    serializer_class = CalendarEventSerializer
+#    def list(self, request):
+#        queryset = CalendarEvent.objects.all()
+#        serializer_class = CalendarEventSerializer(queryset,many=True)
+#        return Response(serializer_class.data)
